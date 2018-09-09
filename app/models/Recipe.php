532f47d46517b9e060824b53e6f9bdae593e3af6
Larry <larry.sell1@gmail.com>
2018-09-09 16:29:38 -0400

    Added Recipe and Meal model classes

---
+++ app/models/Recipe.php
@@ -0,0 +1,31 @@
+<?php
+class Recipe{
+	private $name;
+	private $description;
+	private $yield;
+	private $ingredients;
+	private $source;
+	private $notes;
+
+	public __construct($theName,$theDescription,$theYield,$theSource,$theNotes){
+		$this->name = $theName;
+		$this->description = $theDescription;
+		$this->yield = $theYield;
+		$this->ingredients = array();
+		$this->source = $theSource;
+		$this->notes = $theNotes;
+	}
+	public function addIngredient($anIngredient){
+		$this->ingredient[] = $anIngredient;	
+	}
+	public function removeIngredient($anIngredient){
+		for($i=0;$i<$this->ingredients;$i++){
+			if($this->ingredients[$i] == $anIngredient)  // Or use equality method
+				unset($this->ingredients[$i]);
+		}
+	}
+	public function swapIngredient($old,$new){
+		removeIngredient($old);
+		addIngredient($new);
+	}
+?>
