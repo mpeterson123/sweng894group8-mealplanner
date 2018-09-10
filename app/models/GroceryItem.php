44dfd805c7a995bede7ea8f1f27d732fc6d6d4c2
Andrew Drake <andrewdrake42@gmail.com>
2018-09-09 22:20:52 -0400

    Add files via upload

---
+++ app/models/GroceryItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Base\Models;
+
+class GroceryItem {
+  private $name,
+    $item;
+
+    public function __construct($n, $itm) {
+      $this->name = $fi;
+      $this->item = $itm;
+    }
+
+    public function setName($n) {
+      $this->name = $n;
+    }
+	
+}
+
+?>
