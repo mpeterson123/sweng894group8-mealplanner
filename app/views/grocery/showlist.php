5964cb6755a1fbc7f39f09dc863110a25d7b2661
Andrew Drake <andrewdrake42@gmail.com>
2018-09-23 19:16:37 -0400

    Grocery start
    not tested yet

---
+++ app/views/grocery/showlist.php
@@ -0,0 +1,111 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+// MealPlanner                             Penn State - Cohorts 19 & 20 @ 2018
+///////////////////////////////////////////////////////////////////////////////
+// Food (listing)
+///////////////////////////////////////////////////////////////////////////////
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/../app/views/modules/main.mod.php' );
+
+// Sub Title
+$SUBTITLE = 'Grocery';
+
+// Plugins
+$PLUGIN_SLIMSCROLL  = TRUE;
+$PLUGIN_WAVES       = TRUE;
+$PLUGIN_DATATABLES  = TRUE;
+$PLUGIN_SIDEBARMENU = TRUE;
+$PLUGIN_EXPORT      = TRUE;
+
+// Food
+$User['id'] = 1;  // Default to 1 for testing purposes
+$Foods = sqlRequestWhere('food', 'userid', $User['id']);
+$Recipes = sqlRequestWhere('recipe', 'userid', $User['id']);
+
+?>
+<?php require_once( __HEADER__ ); ?>
+
+<body class="mini-sidebar">
+    <!-- ===== Main-Wrapper ===== -->
+    <div id="wrapper">
+        <div class="preloader">
+            <div class="cssload-speeding-wheel"></div>
+        </div>
+
+<?php require_once( __NAVBAR__ ); ?>
+
+<?php require_once( __SIDEBAR__ ); ?>
+
+        <!-- ===== Page-Content ===== -->
+        <div class="page-wrapper">
+
+            <!-- ===== Page-Container ===== -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="white-box">
+                            <h3 class="box-title m-b-0">Food Directory</h3>
+                            <p class="text-muted m-b-30">Export data to Copy, CSV, Excel, PDF & Print</p>
+                            <div class="table-responsive">
+                                <table id="export-table" class="display nowrap" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Food</th>
+                                            <th>Unit Cost</th>
+                                            <th>Quantity</th>
+                                            <th>Total Cost</th>
+                                        </tr>
+                                    </thead>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Food</th>
+                                            <th>Unit Cost</th>
+                                            <th>Quantity</th>
+                                            <th>Total Cost</th>
+                                        </tr>
+                                    </tfoot>
+                                    <tbody>
+
+<?php
+foreach ($Recipes as $recipe) {
+    if ($food['stock'] < $recipe['']) {
+      $b = $a;
+}
+?>
+                                        <tr>
+                                            <td><a href="/foods/food/<?php echo $food['id']; ?>"><?php echo $food['name']; ?></a></td>
+                                            <td>$<?php echo $food['unitcost']; ?></td>
+                                        </tr>
+
+<?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-2">
+                        <div class="white-box">
+                            <h3 class="box-title m-b-0">Options</h3>
+                            <a href="/foods/add/" class="btn btn-success m-t-15">+ Add Food Item</a>
+                        </div>
+                    </div>
+                </div>
+
+
+<?php require_once( __SPANEL__ ); ?>
+
+            </div>
+            <!-- ===== Page-Container-End ===== -->
+
+            <footer class="footer t-a-c">
+                <?php echo __COPYRITE__; ?>
+            </footer>
+        </div>
+        <!-- ===== Page-Content-End ===== -->
+
+    </div>
+    <!-- ===== Main-Wrapper-End ===== -->
+
+<?php require_once( __FOOTER__ ); ?>
+
+</body>
+</html>
