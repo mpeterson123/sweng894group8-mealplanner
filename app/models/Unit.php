ac68d055222ec8d3a8dcc68f653a8aefffd1b1e5
camilaiperez <cpd.work@gmail.com>
2018-09-30 14:35:26 -0400

    Add unit tests and implementation to Unit class

--- app/models/Unit.php
+++ app/models/Unit.php
@@ -14,27 +14,85 @@ class Unit
         $name,
         $baseEqv;
 
+    public function setId($id)
+    {
+        if(!$id)
+        {
+            throw new \Exception("Id cannot be empty", 1);
+        }
+
+        if(gettype($id) !== 'integer'){
+            throw new \Exception("Id must be an integer", 1);
+        }
+
+        $this->id = $id;
+    }
+
     public function getId()
     {
         return $this->id;
     }
 
-    public function setId($id)
-    {
-        $this->id = $id;
+    //////////
+    // Name //
+    //////////
+    public function setName($name){
+        $name = trim($name);
+
+        /*
+         * Regex rules:
+         * - Only letters and parentheses
+         * - Case insensitive
+         * - From 1-20 characters
+         */
+        $regex = '/^[a-z\(\)]{1,20}$/i';
+
+        if(!preg_match_all($regex, $name, $matches)){
+            throw new \Exception(
+                "Food Item name must only contain letters and parentheses, and must be 1-20 characters in length", 1);
+        }
+
+        $this->name = $name;
     }
 
-    public function getName()
-    {
+    public function getName(){
         return $this->name;
     }
 
-    public function setName($name)
-    {
-        $this->name = $name;
+    //////////////////
+    // Abbreviation //
+    //////////////////
+    public function setAbbreviation($abbreviation){
+        $abbreviation = trim($abbreviation);
+
+        if($abbreviation == ''){
+            throw new \Exception(
+                "Food Item abbreviation cannot be empty", 1);
+        }
+
+        if(strlen($abbreviation) > 4){
+            throw new \Exception(
+                "Food Item abbreviation cannot be longer than 4 characters", 1);
+        }
+
+        if(!preg_match_all('/^[a-z]+$/i', $abbreviation, $matches)){
+            throw new \Exception(
+                "Food Item abbreviation must be alphabetical", 1);
+        }
+
+        $this->abbreviation = $abbreviation;
     }
+
+    public function getAbbreviation(){
+        return $this->abbreviation;
+    }
+
+
     public function setBaseEqv($eqv)
     {
+        if($eqv <= 0){
+            throw new \Exception("Base Equivalence must be greater than 0", 1);
+        }
         $this->baseEqv = $eqv;
     }
     public function getBaseEqv(){
---
+++ tests/models/UnitTest.php
@@ -0,0 +1,171 @@
+<?php
+namespace Base\Test;
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+// Add the classes you are testing
+use Base\Models\Unit;
+
+
+class UnitTest extends TestCase {
+    // Variables to be reused
+    private $unit;
+
+
+    /**
+     * Create instances or whatever you need to reuse in several tests here
+     */
+    public function setUp(){
+        $this->unit = new Unit();
+    }
+
+    /**
+     * Unset any variables you've created
+     */
+    public function tearDown(){
+      unset($this->unit);
+    }
+
+    public function testCreateUnit(){
+
+    	$this->assertInstanceOf(
+            'Base\Models\Unit',
+            new Unit(),
+            'Object must be instance of Unit');
+    }
+
+    ////////
+    // Id //
+    ////////
+    public function testSetAndGetId(){
+        $id = 1;
+        $this->unit->setId($id);
+        $this->assertEquals($this->unit->getId(), $id);
+    }
+
+    public function testIdCannotBeEmpty(){
+        $this->expectException(\Exception::class);
+        $this->unit->setId(NULL);
+    }
+
+    public function testIdIsAnInteger(){
+        $intId = 123;
+        $this->unit->setId($intId);
+        $this->assertInternalType('integer', $this->unit->getId());
+    }
+
+    public function testNonIntIdsAreRejected(){
+        $nonIntId = '123';
+        $this->expectException(\Exception::class);
+        $this->unit->setId($nonIntId);
+    }
+
+    //////////
+    // Name //
+    //////////
+
+    public function testSetAndGetName(){
+        $this->unit->setName('mililiter(s)');
+        $this->assertEquals($this->unit->getName(), 'mililiter(s)');
+    }
+
+    public function testNameCannotBeEmpty(){
+        $this->expectException(\Exception::class);
+        $this->unit->setName('');
+    }
+
+    public function testNameCannotBeLongerThan20Chars(){
+        $longName = '123456789012345678901234567890';
+        $this->expectException(\Exception::class);
+        $this->unit->setName($longName);
+    }
+
+    public function testNameCannotHaveExtraWhitespace(){
+        $nameWithWhitespace = '       mililiter(s)   ';
+        $expectedName =  'mililiter(s)';
+        $this->unit->setName($nameWithWhitespace);
+
+        $this->assertEquals($this->unit->getName(), $expectedName,
+            'Name must be trimmed.');
+    }
+
+    public function testNameMustOnlyContainLettersAndParentheses(){
+        $invalidName = 'a bad name!';
+
+        $this->expectException(\Exception::class);
+        $this->unit->setName($invalidName);
+    }
+
+    //////////////////
+    // Abbreviation //
+    //////////////////
+    public function testSetAndGetAbbreviation(){
+        $this->unit->setAbbreviation('mL');
+        $this->assertEquals($this->unit->getAbbreviation(), 'mL');
+    }
+
+    public function testAbbreviationCannotBeEmpty(){
+        $this->expectException(\Exception::class);
+        $this->unit->setAbbreviation('');
+    }
+
+    public function testAbbreviationCannotBeLongerThan4Chars(){
+        $longAbbreviation = '12345';
+        $this->expectException(\Exception::class);
+        $this->unit->setAbbreviation($longAbbreviation);
+    }
+
+    public function testAbbreviationCannotHaveExtraWhitespace(){
+        $abbreviationWithWhitespace = '       mL   ';
+        $expectedAbbreviation =  'mL';
+        $this->unit->setAbbreviation($abbreviationWithWhitespace);
+
+        $this->assertEquals($this->unit->getAbbreviation(), $expectedAbbreviation,
+            'Abbreviation must be trimmed.');
+    }
+
+    public function testAbbreviationMustBeAlphabetical(){
+        $invalidAbbreviation = 'bad!';
+
+        $this->expectException(\Exception::class);
+        $this->unit->setAbbreviation($invalidAbbreviation);
+    }
+
+    //////////////
+    // Base Eqv //
+    //////////////
+
+    public function testSetAndGetBaseEqv(){
+        $baseEqv = 12345.12345678;
+        $this->unit->setBaseEqv($baseEqv);
+        $this->assertEquals($this->unit->getBaseEqv(), $baseEqv, '', 0.00000001);
+    }
+
+    public function testBaseEqvIsDouble(){
+        $baseEqv = 12345.12345678;
+        $this->unit->setBaseEqv($baseEqv);
+        $this->assertInternalType('double', $this->unit->getBaseEqv());
+    }
+
+    public function testBaseEqvCannotBeNegative(){
+        $negativeBaseEqv = -12345.6789;
+        $this->expectException(\Exception::class);
+        $this->unit->setBaseEqv($negativeBaseEqv);
+    }
+
+    public function testBaseEqvCannotBeZero(){
+        $negativeBaseEqv = 0;
+        $this->expectException(\Exception::class);
+        $this->unit->setBaseEqv($negativeBaseEqv);
+    }
+    //
+    // public function testBaseEqvCannotHaveExtraWhitespace(){
+    //     $abbreviationWithWhitespace = '       mL   ';
+    //     $expectedBaseEqv =  'mL';
+    //     $this->unit->setBaseEqv($abbreviationWithWhitespace);
+    //
+    //     $this->assertEquals($this->unit->getBaseEqv(), $expectedAbbreviation,
+    //         'Abbreviation must be trimmed.');
+    // }
+}
