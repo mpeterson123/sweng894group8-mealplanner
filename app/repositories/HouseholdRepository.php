6a29cc1d0544e8b6a87f07f7e521c3113dcd5a19
camilaiperez <cpd.work@gmail.com>
2018-10-21 13:42:32 -0400

    Suppress bind_param notice and fix newly added static Session call

--- app/repositories/HouseholdRepository.php
+++ app/repositories/HouseholdRepository.php
@@ -33,7 +33,7 @@ class HouseholdRepository extends Repository {
 
     public function allForUser($user){
         $query = $this->db->prepare('SELECT household.* FROM household JOIN usersHouseholds ON usersHouseholds.householdId = household.id WHERE usersHouseholds.userId = ?');
-        $query->bind_param("i",$user->getId());
+        @$query->bind_param("i",$user->getId());
         $query->execute();
         $result = $query->get_result();
 
