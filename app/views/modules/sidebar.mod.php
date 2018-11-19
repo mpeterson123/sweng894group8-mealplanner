9c54cf10f7a660d3fe878bdcb8bf74bc033afa80
Belisarius Smith <bsmith@belisariussmith.com>
2018-11-18 19:47:48 -0600

    Updated sidebar

--- app/views/modules/sidebar.mod.php
+++ app/views/modules/sidebar.mod.php
@@ -6,9 +6,9 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 $sidebarNumUnread  = $NumUnread ?? NULL;
-$sidebarNumFoods   = sqlRequest("SELECT COUNT(id) AS theTotal FROM foods WHERE houseHoldId = {$data['user']->getHouseHoldId()}")[0]['theTotal'];
-$sidebarNumMeals   = sqlRequest("SELECT COUNT(id) AS theTotal FROM meal WHERE houseHoldId = {$data['user']->getHouseHoldId()}")[0]['theTotal'];
-$sidebarNumRecipes = sqlRequest("SELECT COUNT(id) AS theTotal FROM recipes WHERE houseHoldId = {$data['user']->getHouseHoldId()}")[0]['theTotal'];
+$sidebarNumFoods   = sqlRequest("SELECT COUNT(id) AS theTotal FROM foods WHERE houseHoldId = {$data['user']->getHouseholds()[0]}")[0]['theTotal'];
+$sidebarNumMeals   = sqlRequest("SELECT COUNT(id) AS theTotal FROM meal WHERE houseHoldId = {$data['user']->getHouseholds()[0]}")[0]['theTotal'];
+$sidebarNumRecipes = sqlRequest("SELECT COUNT(id) AS theTotal FROM recipes WHERE houseHoldId = {$data['user']->getHouseHolds()[0]}")[0]['theTotal'];
 ?>
         <!-- ===== Left-Sidebar ===== -->
         <aside class="sidebar">
